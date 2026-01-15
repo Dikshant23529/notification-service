a12b1c3a9c5beae39b11a2f22333b83315112460
devsecbox <latherdikshant0@gmail.com>
2026-01-15 12:38:34 +0530

    Now we will use the Mongo DB database for our project.

--- src/main/java/com/talent/graph/notification_service/model/NotificationTemplate.java
+++ src/main/java/com/talent/graph/notification_service/model/NotificationTemplate.java
@@ -1,57 +1,19 @@
 package com.talent.graph.notification_service.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import lombok.*;
 
-
-import java.time.LocalDateTime;
-import java.util.Map;
 import java.util.UUID;
 
 @Entity
 @Table(name = "notification_template")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 public class NotificationTemplate {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false, unique = true)
     private String name;
 
-    private String description;
-
-    @Column(nullable = false, columnDefinition = "TEXT")
-    private String subjectTemplate;
-
-    @Column(nullable = false, columnDefinition = "TEXT")
-    private String bodyTemplate;
-
-    private String category;
-    private String language = "en";
-
-    @ElementCollection
-    @CollectionTable(name = "template_variables",
-            joinColumns = @JoinColumn(name = "template_id"))
-    @MapKeyColumn(name = "variable_name")
-    @Column(name = "variable_description")
-    private Map<String, String> variables;
-
-    @Column(nullable = false)
-    private boolean isActive = true;
-
-    @CreationTimestamp
-    private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
 }
